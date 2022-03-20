aa3b84dbe57fe3f90004adc8a3ad00ff5388c2da
crosslinked <101917795+crosslinked@users.noreply.github.com>
2022-03-20 11:35:26 +0530

    Add files via upload

---
+++ 2code.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main() {
+    int a,b,c;
+   printf("RISHABH JAIN\n");
+   printf("enter the value of a=\n");
+   scanf("%d",&a);
+   printf("enter th valu of b=\n");
+   scanf("%d",&b);
+   c=a+b;
+   printf("sum=%d",c);
+   getch();
+}
